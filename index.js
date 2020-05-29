5319d7abd6e7085bb3b5ae65ffa027e23c34ece4
Shubham Serva <M1047224@mindtree.com>
2020-05-29 10:54:05 +0530

    ALmost final code

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(function(req, res, next) {
    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
  });
-const routes = require('../Airlines-Server/src/routes/routes');
+const routes = require('./src/routes/routes');
 app.use('/',routes);  
 
  //<<------------------USed to add flights data to DB----------------->
